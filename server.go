5fe8a9d39d989058cbd3d3b1c2201d69aa3487e6
26huitailang <26huitailang@gmail.com>
2019-09-15 11:45:03 +0800

    refactor

--- server.go
+++ server.go
@@ -8,10 +8,17 @@ import (
 func ProfileServer(w http.ResponseWriter, r *http.Request) {
 	username := r.URL.Path[len("/profiles/"):]
 
+	fmt.Fprint(w, GetUserProfile(username))
+}
+
+func GetUserProfile(username string) string {
 	if username == "Peter" {
-		fmt.Fprint(w, "Peter's Profile")
+		return "Peter's Profile"
+
 	}
 	if username == "Chris" {
-		fmt.Fprint(w, "Chris's Profile")
+		return "Chris's Profile"
 	}
+
+	return ""
 }
--- server_test.go
+++ server_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -8,28 +9,36 @@ import (
 
 func TestGETProfile(t *testing.T) {
 	t.Run("returns Peter's profile", func(t *testing.T) {
-		request, _ := http.NewRequest(http.MethodGet, "/profiles/Peter", nil)
+		request := newGetProfileRequest("Peter")
 		response := httptest.NewRecorder()
 
 		ProfileServer(response, request)
 		got := response.Body.String()
 		want := "Peter's Profile"
 
-		if got != want {
-			t.Errorf("got %q, want %q", got, want)
-		}
+		assertResponseBody(t, got, want)
 	})
 
 	t.Run("returns Chris's profile", func(t *testing.T) {
-		request, _ := http.NewRequest(http.MethodGet, "/profiles/Chris", nil)
+		request := newGetProfileRequest("Chris")
 		response := httptest.NewRecorder()
 
 		ProfileServer(response, request)
 		got := response.Body.String()
 		want := "Chris's Profile"
 
-		if got != want {
-			t.Errorf("got %q, want %q", got, want)
-		}
+		assertResponseBody(t, got, want)
 	})
+}
+
+func newGetProfileRequest(username string) *http.Request {
+	request, _ := http.NewRequest(http.MethodGet, fmt.Sprintf("/profiles/%s", username), nil)
+	return request
+}
+
+func assertResponseBody(t *testing.T, got, want string) {
+	t.Helper()
+	if got != want {
+		t.Errorf("response body is wrong, got %q, want %q", got, want)
+	}
 }
